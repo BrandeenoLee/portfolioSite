565eb6e9257371b14ba0c0f9a6b031a78a018388
BrandeenoLee <brandonladcock@gmail.com>
2020-11-24 17:03:25 -0600

    Card components and CSS

---
+++ src/components/resume.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
+import Skills from './skills';
+
+
+class Resume extends Component {
+  render() {
+    return(
+      <div>
+        <Grid>
+          <Cell col={4}>
+            <div style={{textAlign: 'center'}}>
+              <img
+                src="https://www.shareicon.net/download/2015/09/18/103157_man_512x512.png"
+                alt="avatar"
+                style={{height: '200px'}}
+                 />
+            </div>
+
+            <h2 style={{paddingTop: '2em'}}>Paul Hanna</h2>
+            <h4 style={{color: 'grey'}}>Programmer</h4>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <p>Text Here</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+            <h5>Address</h5>
+            <p>1 Hacker Way Menlo Park</p>
+            <h5>Phone</h5>
+            <p>(123) 456-7890</p>
+            <h5>Email</h5>
+            <p>someone@example.com</p>
+            <h5>Web</h5>
+            <p>mywebsite.com</p>
+            <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
+          </Cell>
+          <Cell className="resume-right-col" col={8}>
+            
+              <h2>Skills</h2>
+              <Skills
+                skill="javascript"
+                progress={100}
+                />
+                <Skills
+                  skill="HTML/CSS"
+                  progress={80}
+                  />
+                  <Skills
+                    skill="NodeJS"
+                    progress={50}
+                    />
+                    <Skills
+                      skill="React"
+                      progress={25}
+                      />
+
+
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default Resume;
