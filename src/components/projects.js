08965f68f9dd11e6591f144533c341326bb70c91
BrandeenoLee <brandonladcock@gmail.com>
2020-12-10 16:57:52 -0600

    added screenshots in assests.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import covidTrackerSS from '../assets/covidTrackerSS.png'
+import covidTrackerSS from '../assets/covidTrackerSS.png';
+import weatherAppSS from '../assets/weatherAppSS.png';
+import recipeAppSS from '../assets/recipeAppSS.png'
 import { Grid, Cell } from 'react-mdl';
 
 class Projects extends Component {
@@ -16,41 +18,61 @@ render() {
     return(
       <div className="projectsContainer">
       <Grid>
-        <Cell col={4}>
-        <h1>COVID-19 Tracker</h1>
-        <img
-          src={covidTrackerSS}
-          style={{height: '250px'}}/>
+        <Cell className='cell1'col={4}>
+
+          <img
+            src={covidTrackerSS}
+            style={{height: '250px'}}/>
         </Cell>
         <Cell col={8}>
+              <h1>COVID-19 Tracker</h1>
               <p>Displays information in a graph using an API that grabs current COVID-19 data.</p>
-              <a href='https://github.com/BrandeenoLee/covid-tracker'></a>
-              <i class="fab fa-github icon"></i>
-              <p>deployed link</p>
+              <a href='https://github.com/BrandeenoLee/covid-tracker'>
+                <i class="fab fa-github icon"></i>
+                  github.com/BrandeenoLee/covid-tracker
+              </a>
+              <div>
+                 <a href='https://globalcovidstats.netlify.app/'>
+                 <i class="fas fa-external-link-alt"></i>
+                    globalcovidstats.netlify.app/
+                </a>
+              </div>
+              
         </Cell>
         <Cell col={4}>
-          <h1>Weather App</h1>
+  
               <img
-                src={covidTrackerSS}
+                src={recipeAppSS}
                 style={{height: '250px'}}/>
         </Cell>
         <Cell col={8}>
+              <h1>Recipe Search App</h1>
+              <p>Use API to return recipes by keyword search.</p>
+              <a href='https://github.com/BrandeenoLee/reactRecipeApp'>
+                <i class="fab fa-github icon"></i>
+                  github.com/BrandeenoLee/reactRecipeApp
+              </a>
+
+        </Cell>
+        <Cell col={4}>
+
+              <img
+                src={weatherAppSS}
+                style={{height: '250px'}}/>
+        </Cell>
+        <Cell col={8}>
+              <h1>Weather App</h1>
               <p>Uses an API to return weather data requested by input ing a zipcode.</p>
-              <a href='https://github.com/BrandeenoLee/covid-tracker'></a>
-              <i class="fab fa-github icon"></i>
-              <p>deployed link</p>
-        </Cell>
-        <Cell col={4}>
-          <h1>Recipe App</h1>
-              <img
-                src={covidTrackerSS}
-                style={{height: '250px'}}/>
-        </Cell>
-        <Cell col={4}>
-              <p>Displays information in a graph using an API that grabs current COVID-19 data.</p>
-              <a href='https://github.com/BrandeenoLee/covid-tracker'></a>
-              <i class="fab fa-github icon"></i>
-        <p>deployed link</p>
+              <a href='https://github.com/BrandeenoLee/weather-app'>
+                <i class="fab fa-github icon"></i>
+                  github.com/BrandeenoLee/weather-app
+              </a>
+              <div>
+                 <a href='https://weathersearchbyzip.netlify.app/'>
+                 <i class="fas fa-external-link-alt"></i>
+                    weathersearchbyzip.netlify.app/
+                </a>
+              </div>
         </Cell>
       </Grid>
       </div>
