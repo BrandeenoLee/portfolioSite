b3ba53340f369aea9e03ccecdba52be0e00ffd14
BrandeenoLee <brandonladcock@gmail.com>
2021-01-11 19:02:08 -0600

    added image gallery.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import contactPicture from "../assets/contactimage.jpg";
+import prizedCow from "../assets/prizedCow.jpg";
+import poofNeck from "../assets/poofNeck.jpg";
+import itsBits from "../assets/itsBits.jpg";
+import { Carousel } from "react-bootstrap";
 
 class Contact extends Component {
   render() {
     return (
       <div className="projectsGrid">
         <div className="image">
-          <img src={contactPicture} alt="brandon"/>
+          <img src={contactPicture} alt="brandon" />
         </div>
         <div className="description">
           <h2>Get in touch!</h2>
@@ -19,6 +23,20 @@ class Contact extends Component {
             <i className="fab fa-github icon"></i>
             github.com/BrandeenoLee
           </a>
+        </div>
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={prizedCow} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={poofNeck} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={itsBits} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+        <div className="description">
+          My Animals like are excellent at helping me test!
         </div>
       </div>
     );
